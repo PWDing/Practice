1eaf9d75f2948a6e5ef1f9c8e73f93eaf3908ed4
plutors <xpwding@gmail.com>
2019-05-16 17:08:04 +0800

    try to solve sudoku

--- sudoku.py
+++ sudoku.py
@@ -1,19 +1,6 @@
 import random
 
 
-def generate_possible_matrix():
-    puzzle = [
-        [0, 0, 0, 2, 1, 4, 0, 0, 0],
-        [1, 0, 0, 0, 0, 0, 0, 0, 9],
-        [0, 6, 4, 0, 0, 0, 5, 2, 0],
-        [0, 9, 0, 3, 0, 7, 0, 6, 0],
-        [0, 0, 6, 0, 0, 0, 4, 0, 0],
-        [0, 8, 0, 0, 6, 0, 0, 9, 0],
-        [5, 1, 0, 0, 2, 0, 0, 8, 3],
-        [8, 0, 0, 0, 3, 0, 0, 0, 4],
-        [0, 0, 0, 7, 0, 8, 0, 0, 0]
-    ]
-
 # Cross product of elements in A and elements in B.
 def cross(list_a, list_b):
     return [a + b for b in list_b for a in list_a]
@@ -32,11 +19,14 @@ cells = cross(rows, cols)
 # 生成所有的单元和行、列
 unitlist = ([cross(rows, c) for c in cols] +
             [cross(r, cols) for r in rows] +
-            [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')])
+            [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') 
+            for cs in ('123', '456', '789')])
 # 对每一个方格，求出与它相关的单元
-units = dict((cell, [unit for unit in unitlist if cell in unit]) for cell in cells)
+units = dict((cell, [unit for unit in unitlist if cell in unit]) 
+             for cell in cells)
 # 对每一个方格，求出它所有的搭档
-peers = dict((cell, set([cell for unit in units[cell] for cell in unit])) for cell in cells)
+peers = dict((cell, set([cell for unit in units[cell] for cell in unit])) 
+             for cell in cells)
 
 
 # Convert grid into a dict of {cell: digit_string} with '0' or '.' for empty.
@@ -49,33 +39,40 @@ def convert_grid_to_dict(grid):
 # Convert grid to a dict of possible values, {cell: digits}
 # or return False if a contradiction is detected.
 def parse_grid(grid):
-    # To start, every square can be any digit; then assign values from the grid.
+    # To start, every square can be any digit
+    # then assign values from the grid.
     grid_dict = dict((cell, digits) for cell in cells)
     for c, d in convert_grid_to_dict(grid).items():
         # Fail if we can't assign digit d to cell c
-        if d in digits:
-            grid_dict[c] = d
+        if d in digits and not assign(grid_dict, c, d):
+            return False
     return grid_dict
 
-"""
-Eliminate all the other values (except digit) from dicts[cell] and propagate.
-Return dicts, except return False if a contradiction is detected.
- """
+
 def assign(dicts, cell, digit):
-    pass
+    """
+    Eliminate all the other values (except digit) from dicts[cell] and propagate.
+    Return dicts, except return False if a contradiction is detected.
+    """
+    other_dicts = dicts[cell].replace(digit, '')
+    if all(eliminate(dicts, cell, v) for v in other_dicts):
+        return dicts
+    else:
+        return False
 
 
-"""
-Eliminate d from dicts[c]; propagate when values or places <= 2.
-Return dicts, return False except if a contradiction is detected.
-"""
 def eliminate(dicts, c, d):
+    """
+    Eliminate d from dicts[c]; propagate when values or places <= 2.
+    Return dicts, return False except if a contradiction is detected.
+    """
     # Already eliminated
     if d not in dicts[c]:
         return dicts
     dicts[c] = dicts[c].replace(d, '')
 
-    # If a square c is reduced to one value v, then eliminate d from the peers.
+    # If a cell c is reduced to one value v
+    # then eliminate value d from the peers.
     if len(dicts[c]) == 0:
         # COntradiction: removed all values
         return False
@@ -84,5 +81,30 @@ def eliminate(dicts, c, d):
         if not all(eliminate(dicts, p, v) for p in peers[c]):
             return False
 
+    # If a unit u is reduced to only one place for a value d
+    # then put it there.
+    for u in units[c]:
+        places = [c for c in u if d in dicts[c]]
+        # Contradiction: no place for this value
+        if len(places) == 0:
+            return False
+        elif len(places) == 1:
+            # d can only be in one place in unit, assign it there
+            if not assign(dicts, places[0], d):
+                return False
+    return dicts
 
-print(parse_grid('4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'))
+
+# Display these values as a 2-D grid.
+def display(dicts):
+    width = max(len(dicts[cell]) for cell in cells) + 1
+    line = '+'.join(['-' * (width * 3)] * 3)
+    for row in rows:
+        print(''.join(dicts[row + col].center(width) +
+              ('|' if col in '369' else '')for col in cols))
+        if row in 'CFI':
+            print(line)
+
+
+print(display(parse_grid('4.....8.5.3..........7......2....\
+    .6.....8.4......1.......6.3.7.5..2.....1.4......')))
