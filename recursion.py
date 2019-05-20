5724a9d6a9f024c71173f8d51d1ed878c7fcd0ec
plutors <xpwding@gmail.com>
2019-05-20 16:14:24 +0800

    add hanoi solution

--- recursion.py
+++ recursion.py
@@ -56,7 +56,7 @@ def draw_spiral(turtle_obj, line_len):
         draw_spiral(turtle_obj, length+5)
 
 
-def draw_fractal_tree(branch_len, turtle_obj, pensize):
+def draw_fractal_tree(turtle_obj, branch_len, pensize):
     if branch_len > 5:
         if pensize > 0:
             turtle_obj.pensize(pensize)
@@ -64,15 +64,58 @@ def draw_fractal_tree(branch_len, turtle_obj, pensize):
         angle = random.randrange(15, 45, 5)
         sub_len = random.randrange(10, 20)
         turtle_obj.right(angle)
-        draw_fractal_tree(branch_len-sub_len, turtle_obj, pensize-1)
+        draw_fractal_tree(turtle_obj, branch_len-sub_len, pensize-1)
         turtle_obj.left(2*angle)
-        draw_fractal_tree(branch_len-sub_len, turtle_obj, pensize-1)
+        draw_fractal_tree(turtle_obj, branch_len-sub_len, pensize-1)
         turtle_obj.right(angle)
         if branch_len < 20:
             turtle_obj.color("green")
         else:
             turtle_obj.color("brown")
         turtle_obj.backward(branch_len)
+
+
+def draw_sierpinski(pen, points, degree):
+    colors = ['blue', 'red', 'green', 'white', 'yellow',
+              'violet', 'orange']
+    draw_triangle(pen, points, colors[degree])
+    if degree > 0:
+        draw_sierpinski(pen, [points[0],
+                              get_mid(points[0], points[1]),
+                              get_mid(points[0], points[2])],
+                        degree-1)
+        draw_sierpinski(pen, [points[1],
+                              get_mid(points[1], points[0]),
+                              get_mid(points[1], points[2])],
+                        degree-1)
+        draw_sierpinski(pen, [points[2],
+                              get_mid(points[2], points[0]),
+                              get_mid(points[2], points[1])],
+                        degree-1) 
+
+
+def get_mid(pos1, pos2):
+    return [(pos1[0]+pos2[0])/2, (pos1[1]+pos2[1])/2]
+
+
+def draw_triangle(pen, points, color):
+    pen.fillcolor(color)
+    pen.hideturtle()
+    pen.up()
+    pen.goto(points[0][0], points[0][1])
+    pen.down()
+    pen.begin_fill()
+    pen.goto(points[1][0], points[1][1])
+    pen.goto(points[2][0], points[2][1])
+    pen.goto(points[0][0], points[0][1])
+    pen.end_fill()
+
+
+def hanoi(n, from_rod, aux_rod, to_rod):
+    if n >= 1:
+        hanoi(n-1, from_rod, to_rod, aux_rod)
+        print("move top disk from {} to {}.".format(from_rod, to_rod))
+        hanoi(n-1, aux_rod, from_rod, to_rod)
 
 
 if __name__ == '__main__':
@@ -90,13 +133,18 @@ if __name__ == '__main__':
     # print(is_palindrome('Kanakanak – a town in Alaska'))
     # print(is_palindrome('Wassamassaw – a town in South Dakota'))
 
-    my_turtle = turtle.Turtle()
-    my_screen = turtle.Screen()
-    # draw_spiral(my_turtle, 10)
-    my_turtle.left(90)
-    my_turtle.up()
-    my_turtle.backward(100)
-    my_turtle.down()
-    my_turtle.color("brown")
-    draw_fractal_tree(75, my_turtle, 5)
-    my_screen.exitonclick()
+    # my_pen = turtle.Turtle()
+    # my_screen = turtle.Screen()
+    # draw_spiral(my_pen, 10)
+    # my_pen.left(90)
+    # my_pen.up()
+    # my_pen.backward(100)
+    # my_pen.down()
+    # my_pen.color("brown")
+    # draw_fractal_tree(my_pen, 75, 5)
+    # my_screen.exitonclick()
+
+    # my_points = [[-100, -50], [0, 100], [100, -50]]
+    # draw_sierpinski(my_pen, my_points, 5)
+
+    # hanoi(4, 'A', 'B', 'C')
