9bac9ca9e3409c59bda915579d7a33acfd71f32d
plutors <xpwding@gmail.com>
2019-06-16 18:18:36 +0800

    add some data visiualization content

--- DataVisualization/github_data.py
+++ DataVisualization/github_data.py
@@ -29,7 +29,8 @@ class DataVisualization:
             project_stars.append(data_sets['stargazers_count'])
 
         git_style = ls('#333366', base_style=lcs)
-        chart = pygal.Bar(style=git_style, x_label_rotation=45, show_legend=False)
+        chart = pygal.Bar(style=git_style,
+                          x_label_rotation=45, show_legend=False)
         chart.title = 'Most-Starred Python Projects on GitHub'
         chart.x_labels = project_names
 
@@ -39,6 +40,7 @@ class DataVisualization:
 
 if __name__ == '__main__':
     my_data_visual = DataVisualization()
-    myurl = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+    myurl = 'https://api.github.com/search/repositories'\
+            '?q=language:python&sort=stars'
     date_set = my_data_visual.get_web_data(myurl)
     my_data_visual.display_stars(date_set)
